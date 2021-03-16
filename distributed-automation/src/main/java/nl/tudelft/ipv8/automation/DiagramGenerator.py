408d9ca4933a73c6a559269a19a66d822438e20a
Joost Verbraeken <j.verbraeken@student.tudelft.nl>
2021-03-16 17:23:32 +0100

    Update EvaluationParser and DiagramGenerator

--- distributed-automation/src/main/java/nl/tudelft/ipv8/automation/DiagramGenerator.py
+++ distributed-automation/src/main/java/nl/tudelft/ipv8/automation/DiagramGenerator.py
@@ -12,44 +12,112 @@ for gui in gui_env:
 print("Using:",matplotlib.get_backend())
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
-pdf2 = pd.read_csv("C:/Users/jverb/Downloads/evaluations/parsed - evaluation-simulated-2021-01-02_20.43.03.csv")
+pdf2 = pd.read_csv("C:/Users/jverb/Downloads/evaluations/parsed - evaluation-simulated-2021-03-14_23.07.21.csv")
 
 columns = pdf2.columns
-count = 0
-diagram = 0
 diagram_names = [
-    "Our node is twice as slow as the other nodes",
-    "Our node is twice as fast as the other nodes",
+    "Our node is 5x as slow as the other nodes",
+    "Our node is 5x as fast as the other nodes",
     "Our node joins late",
-    "Data is non-i.i.d. (each node has 50% of the data)",
-    "MNIST - 5 nodes",
-    "CIFAR-10 - 5 nodes",
-    "HAR - 5 nodes",
-    "MNIST - 4 nodes",
-    "MNIST - 5 nodes",
-    "MNIST - 6 nodes",
-    "MNIST - 5 nodes + 2 noise attackers",
-    "MNIST - 5 nodes + 2 label-flip attackers",
-    "MNIST - 5 nodes + 2 Fang (2020) KRUM attackers",
-    "MNIST - 5 nodes + 2 Fang (2020) Trimmed Mean attackers",
-    "MNIST - 5 nodes + 1 noise attacker",
-    "MNIST - 5 nodes + 2 noise attackers",
-    "MNIST - 5 nodes + 3 noise attackers",
+
+    "MNIST - 10 nodes",
+    "CIFAR-10 - 10 nodes",
+    "WISDM - 10 nodes",
+
+    "MNIST - 10 nodes + 4 noise attackers",
+    "CIFAR-10 - 10 nodes + 4 noise attackers",
+    "WISDM - 10 nodes + 4 noise attackers",
+
+    "MNIST - 10 nodes + 4 2-label-flip attackers",
+    "MNIST - 10 nodes + 4 all-label-flip attackers",
+    "MNIST - 10 nodes + 2 Fang (2020) KRUM attackers",
+    "MNIST - 10 nodes + 2 Fang (2020) Trimmed Mean attackers",
+
+    "MNIST - 10 nodes non-i.i.d. + 4 2-label-flip attackers",
+    "MNIST - 10 nodes non-i.i.d. + 4 all-label-flip attackers",
+    "MNIST - 10 nodes non-i.i.d. + 2 Fang (2020) KRUM attackers",
+    "MNIST - 10 nodes non-i.i.d. + 2 Fang (2020) Trimmed Mean attackers",
+
+    "MNIST - 10 nodes non-i.i.d. + 2 noise attackers",
+    "MNIST - 10 nodes non-i.i.d. + 4 noise attackers",
+    "MNIST - 10 nodes non-i.i.d. + 8 noise attackers",
 ]
-for column in [column for column in pdf2.columns.tolist() if not column[0:7] == "Unnamed" and column != ' ' and column[1] != '.']:
-    accuracies = [float(i) for i in pdf2[column].tolist() if i != ' ']
-    iterations = [i * 10 for i in list(range(len(accuracies)))]
-    if diagram == 2:
-        iterations = [i + 100 for i in iterations]
-    plt.plot(iterations, accuracies, label = column, linewidth = 4 if "bristle" in column else 2)
-    count += 1
-    if count % 6 == 0:
-        plt.xlim(0, iterations[-1])
-        plt.ylim(0, 1)
-        plt.legend()
-        plt.title(diagram_names[diagram])
-        # plt.show()
-        plt.savefig("C:/Users/jverb/Downloads/evaluations/" + column.split(" - ")[0] + ".png")
-        plt.clf()
+plt_regular_data = []
+plt_regular_data_all = []
+plt_transfer_data = []
+plt_transfer_data_all = []
+plt_bound_data = defaultdict(lambda: {})
+skip = False
+colors = {
+    "average": "darkviolet",
+    "bridge": "orange",
+    "bristle": "green",
+    "krum": "red",
+    "median": "royalblue",
+    "mozi": "brown"
+}
+diagram = 0
+for column in [column for column in pdf2.columns.tolist() if not column[0:7] == "Unnamed"]:
+    if skip:
+        skip = False
+        continue
+    if column == ' ' or column[1] == '.':
+        skip = True
+
+        if len(plt_regular_data) > 0:
+            plt_regular_data_all.append(plt_regular_data.copy())
+            plt_regular_data.clear()
+
+        if len(plt_transfer_data) > 0:
+            plt_transfer_data_all.append(plt_transfer_data.copy())
+            plt_transfer_data.clear()
         diagram += 1
+    else:
+        accuracies = [float(i) for i in pdf2[column].tolist() if i != ' ']
+        iterations = [i * 10 for i in list(range(len(accuracies)))]
+        if "Bound" in column:
+            split = column.split(" - ")
+            plt_bound_data[split[0]][split[2]] = (iterations, accuracies, column)
+        else:
+            if diagram == 2:
+                iterations = [i + 150 for i in iterations]
+            if "regular" in column:
+                plt_regular_data.append((iterations, accuracies, column, 4 if "bristle" in column else 2, column.split(" - ")[1]))
+            if "transfer" in column:
+                plt_transfer_data.append((iterations, accuracies, column, 4 if "bristle" in column else 2, column.split(" - ")[1]))
+
+for (diagram, data) in enumerate(plt_regular_data_all):
+    for (a, b, c, d, e) in data:
+        plt.plot(a, b, label=c, linewidth=d, color=colors[e])
+    if diagram > 2:
+        print(data[-1][2].split(" - ")[0] + " => " + diagram_names[diagram])
+        bound = plt_bound_data[" Figure MinBound " + diagram_names[diagram].split(" - ")[0] + " _ " + ("noniid" if "non-i.i.d." in diagram_names[diagram] else "full")]
+        plt.plot(bound["regular"][0], bound["regular"][1], ":", label="Local run limited dataset", linewidth=2, color="black")
+        bound = plt_bound_data[" Figure MaxBound " + diagram_names[diagram].split(" - ")[0] + " _ " + ("noniid" if "non-i.i.d." in diagram_names[diagram] else "full")]
+        plt.plot(bound["regular"][0], bound["regular"][1], "--", label="Local run full dataset", linewidth=1, color="black")
+    plt.xlim(0, 300)
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.title(diagram_names[diagram])
+    # plt.show()
+    plt.savefig("C:/Users/jverb/Downloads/evaluations/" + data[-1][2].split(" - ")[0] + " - regular.png")
+    plt.clf()
+
+for (diagram, data) in enumerate(plt_transfer_data_all):
+    for (a, b, c, d, e) in data:
+        plt.plot(a, b, label=c, linewidth=d, color=colors[e])
+    if diagram > 2:
+        print(data[-1][2].split(" - ")[0] + " => " + diagram_names[diagram])
+        bound = plt_bound_data[" Figure MinBound " + diagram_names[diagram].split(" - ")[0] + " _ " + ("noniid" if "non-i.i.d." in diagram_names[diagram] else "full")]
+        plt.plot(bound["transfer"][0], bound["transfer"][1], ":", label="Min bound", linewidth=2, color="black")
+        bound = plt_bound_data[" Figure MaxBound " + diagram_names[diagram].split(" - ")[0] + " _ " + ("noniid" if "non-i.i.d." in diagram_names[diagram] else "full")]
+        plt.plot(bound["transfer"][0], bound["transfer"][1], "--", label="Max bound", linewidth=1, color="black")
+    plt.xlim(0, 300)
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.title(diagram_names[diagram])
+    # plt.show()
+    plt.savefig("C:/Users/jverb/Downloads/evaluations/" + data[-1][2].split(" - ")[0] + " - transfer.png")
+    plt.clf()
--- distributed-automation/src/main/java/nl/tudelft/ipv8/automation/EvaluationParser.kt
+++ distributed-automation/src/main/java/nl/tudelft/ipv8/automation/EvaluationParser.kt
@@ -1,18 +1,34 @@
 package nl.tudelft.ipv8.automation
 
+import mu.KotlinLogging
 import java.io.File
 import java.nio.file.Paths
+import java.util.*
 
-private const val STEP_SIZE = 5
+private const val STEP_SIZE = 10
+
+private val logger = KotlinLogging.logger("EvaluationParser")
 
 fun main() {
     val evaluationsFolder = Paths.get(System.getProperty("user.home"), "Downloads", "evaluations").toFile()
     val files = evaluationsFolder.listFiles()!!
-    val mostRecentEvaluations = files
+    val mostRecentEvaluations = files.filter { it.isDirectory }.map {
+        it.listFiles().first { it.isDirectory }.listFiles()
+            .filter { file -> file.extension == "csv" && !file.name.contains("meta") }.maxByOrNull {
+                val split = it.name.split('-')
+                Date(
+                    split[2].toInt(),
+                    split[3].toInt(),
+                    split[4].substring(0, 2).toInt(),
+                    split[4].substring(3, 5).toInt(),
+                    split[4].substring(6, 8).toInt()
+                )
+            }!!
+    }/*files
         .filter { !it.name.startsWith("parsed - ") }
         .filter { !it.isDirectory }
         .sortedByDescending { it.lastModified() }
-        .take(4)
+        .take(4)*/
 
     // Mapping a figure to a mapping of an iteration to its accuracy
     val data = mutableMapOf<String, MutableMap<Int, Double>>()
@@ -29,7 +45,7 @@ fun main() {
             .groupBy { it.key.split(" - ")[0] }
         val header = entries
             .values
-            .map { group -> listOf(*(group.map { it.key }.toTypedArray()), null) }
+            .map { group -> listOf(*(group.map { it.key }.toTypedArray()), null, null) }
             .flatten()
             .map { it ?: "" }
             .toTypedArray()
@@ -43,16 +59,22 @@ fun main() {
 }
 
 fun scanEvaluation(evaluation: File): MutableMap<String, MutableMap<Int, Double>> {
+    logger.debug { evaluation.absolutePath }
     val lines = evaluation.readLines()
 
     val data = mutableMapOf<String, MutableMap<Int, Double>>()
-    for (line in lines.subList(1, lines.size)) {
-        val split = line.split(", ")
-        val figure = split[0]
-        val iteration = split[4].toInt()
-        val accuracy = split[5].toDouble()
-        data.putIfAbsent(figure, mutableMapOf())
-        data[figure]!![iteration] = accuracy
+
+    if (lines.size > 0) {
+        for (line in lines.subList(1, lines.size)) {
+            val split = line.split(", ")
+            if (split[1] == "0") {
+                val figure = split[0]
+                val iteration = split[4].toInt()
+                val accuracy = split[5].toDouble()
+                data.putIfAbsent(figure, mutableMapOf())
+                data[figure]!![iteration] = accuracy
+            }
+        }
     }
     return data
 }
